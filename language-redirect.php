aaa6615141c5e5256b5d6519fe5581d121b19cb8
maksoftnet <mk@maksoft.net>
2024-05-14 20:13:03 +0300

    fixed n= redirect

--- language-redirect.php
+++ language-redirect.php
@@ -16,45 +16,12 @@ function language_redirect() {
     // Проверка за URL параметър 'n'
     $page_number = isset($_GET['n']) ? $_GET['n'] : null;
 
-    // Проверка за наличие на параметър 'n' и редирект
+    // Проверка за наличие на параметър 'n' и различен хост от 'usb-travel.com'
     if (!is_null($page_number) && $_SERVER['HTTP_HOST'] !== 'usb-travel.com') {
         wp_redirect('https://usb-travel.com/page.php?n=' . $page_number);
         exit;
     }
-
 }
-
-function custom_language_switcher($items, $args) {
-    // Проверка дали това е главното меню
-    if ($args->theme_location == 'primary-menu') {
-        // Масив с езиците и техните URL адреси
-        $languages = array(
-            'en' => 'https://en.usb-travel.com',
-            'bg' => 'https://bg.usb-travel.com',
-            'ru' => 'https://ru.usb-travel.com'
-            'fr' => 'https://fr.usb-travel.com'
-        );
-
-        // Вземане на текущия език на сайта
-        $current_language = substr(get_bloginfo('language'), 0, 2);
-
-        // Изграждане на HTML линковете за езиците
-        $language_links = '';
-        foreach ($languages as $language => $url) {
-            // Активен клас за текущия език
-            $active_class = ($language === $current_language) ? 'active' : '';
-            $language_links .= '<li class="menu-item ' . $active_class . '"><a href="' . $url . '">' . $language . '</a></li>';
-        }
-
-        // Добавяне на езиковите линкове към менюто
-        $items .= $language_links;
-    }
-
-    return $items;
-}
-
-// Добавяне на функцията към хука wp_nav_menu_items
-add_filter('wp_nav_menu_items', 'custom_language_switcher', 10, 2);
 
 add_action('template_redirect', 'language_redirect');
 
